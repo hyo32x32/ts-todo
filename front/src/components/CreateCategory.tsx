7f4b7019d7a4b68a42836b0fd3e2c6b6f5f49e1f
hyo32x32 <hyo32x32@gmail.com>
2022-09-11 23:55:01 +0900

    Delete unused files

--- front/src/components/CreateCategory.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { Categories, categoryState, toDoSelector } from "../atoms";
-import styled from "styled-components";
-
-function CreateCategory() {}
-
-export default CreateCategory;
